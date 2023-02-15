18c18d0c3b72fa0c92896a313cf6d0c905a2aa52
ChoonB <shgkdc@gmail.com>
2023-02-15 13:22:47 +0900

    9번 solve

---
+++ Coding Test/Programmers/230215 day1/핸드폰번호가리기.java
@@ -0,0 +1,28 @@
+package day1;
+
+//문제 설명
+//프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
+//전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
+//
+//제한 조건
+//phone_number는 길이 4 이상, 20이하인 문자열입니다.
+
+public class 핸드폰번호가리기 {
+    public static void main(String[] args) {
+        String phone_number = "022345323432";
+        String answer = "";
+
+        answer = "*".repeat((phone_number.length()-4)) + phone_number.substring(phone_number.length()-4,phone_number.length());
+        System.out.println("answer = " + answer);
+
+    }
+}
+
+
+class Solution9 {
+    public String solution(String phone_number) {
+        String answer = "";
+        answer = "*".repeat((phone_number.length()-4)) + phone_number.substring(phone_number.length()-4,phone_number.length());
+        return answer;
+    }
+}
