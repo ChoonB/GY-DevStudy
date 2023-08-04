1213febd14d060af00e64a8b32a7cab7426f1093
ChoonB <shgkdc@gmail.com>
2023-08-04 10:54:36 +0900

    [#1] 유니온파인드 코드 구현

---
+++ CS/Algorithm & Data structure/알고리즘 java로 구현/A_UnionFind.java
@@ -0,0 +1,26 @@
+class A_UnionFind {
+    private int[] parent;
+
+    public UnionFind(int size) {
+        parent = new int[size];
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            parent[rootX] = rootY;
+        }
+    }
+}
