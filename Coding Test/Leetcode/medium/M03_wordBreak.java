f98f02156bb839d9390306e14cb24efd74bbe393
KGY\shgkd <shgkdc@gmail.com>
2024-03-09 00:12:24 +0900

    [#1] Leetcode medium - 03. Word Break

---
+++ Coding Test/Leetcode/medium/M03_wordBreak.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.*;
+
+public class M03_wordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+      Set<String> wordSet = new HashSet<>(wordDict);
+
+
+      boolean[] dp = new boolean[s.length() + 1];
+      dp[0] = true;
+
+
+      for (int i = 1; i <= s.length(); i++) {
+        for (int j = 0; j < i; j++) {
+          if (dp[j] && wordSet.contains(s.substring(j, i))) {
+            dp[i] = true;
+            break;
+          }
+        }
+      }
+
+      return dp[s.length()];
+    }
+}
