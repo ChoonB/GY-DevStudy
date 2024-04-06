3522863c3e56b90bc8d2628faec1f04a8bb79a98
KGY\shgkd <shgkdc@gmail.com>
2024-04-06 21:27:55 +0900

    [#1] Leetcode medium - 10. Count Primes

---
+++ Coding Test/Leetcode/medium/M10_countPrimes.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class M10_countPrimes {
+  public int countPrimes(int n) {
+    boolean[] isPrime = new boolean[n];
+    for (int i = 2; i < n; i++) {
+      isPrime[i] = true;
+    }
+
+    for (int i = 2; i * i < n; i++) {
+      if (isPrime[i]) {
+        for (int j = i * i; j < n; j += i) {
+          isPrime[j] = false;
+        }
+      }
+    }
+
+    int count = 0;
+    for (int i = 2; i < n; i++) {
+      if (isPrime[i]) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+}
